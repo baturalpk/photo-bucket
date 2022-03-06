0cc9167aca8cbc1b95bf5ca3b5355c25587536d4
baturalpk <baturalp.kiziltan@outlook.com>
2022-03-07 00:31:20 +0300

    feat(profile/handlers): Implement handler methods
    [+] Introduces new dependency: gofiber/v2

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	entgo.io/ent v0.10.1
 	github.com/aws/aws-sdk-go v1.43.12
 	github.com/go-playground/validator/v10 v10.10.0
+	github.com/gofiber/fiber/v2 v2.28.0
 	github.com/golang-jwt/jwt/v4 v4.3.0
 	github.com/google/uuid v1.3.0
 	github.com/jackc/pgx/v4 v4.15.0
---
+++ services/profile/handlers.go
@@ -0,0 +1,69 @@
+package profile
+
+import (
+	"github.com/baturalpk/photo-bucket/ent"
+	"github.com/baturalpk/photo-bucket/pkg/validator"
+	"github.com/baturalpk/photo-bucket/services/profile/contracts"
+	"github.com/gofiber/fiber/v2"
+)
+
+type handlers struct {
+	repo *repository
+}
+
+type authProfile struct {
+	Profile     ent.Profile `json:"profile"`
+	TokenString string      `json:"token_string"`
+}
+
+func NewHandlers(profileRepo *repository) *handlers {
+	return &handlers{
+		repo: profileRepo,
+	}
+}
+
+func (h handlers) signUp(c *fiber.Ctx, form interface{}) error {
+	if err := c.BodyParser(form); err != nil {
+		return err
+	}
+	if err := validator.GetValidatorInstance().Struct(form); err != nil {
+		return err
+	}
+	return h.repo.SignUp(c.UserContext(), form)
+}
+
+func (h handlers) SignUpViaEmail(c *fiber.Ctx) error {
+	form := new(contracts.SignUpFormWithEmail)
+	return h.signUp(c, form)
+}
+
+func (h handlers) SignUpViaPhone(c *fiber.Ctx) error {
+	form := new(contracts.SignUpFormWithPhone)
+	return h.signUp(c, form)
+}
+
+func (h handlers) SignIn(c *fiber.Ctx) error {
+	f := new(contracts.SignInForm)
+	if err := c.BodyParser(f); err != nil {
+		return err
+	}
+	if err := validator.GetValidatorInstance().Struct(f); err != nil {
+		return err
+	}
+	profile, token, err := h.repo.SignIn(c.UserContext(), f)
+	if err != nil {
+		return err
+	}
+	return c.JSON(authProfile{
+		Profile:     *profile,
+		TokenString: token,
+	})
+}
+
+func (h handlers) GetByUsername(c *fiber.Ctx) error {
+	profile, err := h.repo.GetByUsername(c.UserContext(), c.Params("username"))
+	if err != nil {
+		return err
+	}
+	return c.JSON(*profile)
+}
