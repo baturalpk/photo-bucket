c7e24a29ce6156cf576b39d5f91334a295906508
baturalpk <baturalp.kiziltan@outlook.com>
2022-03-13 01:59:59 +0300

    Implement cmd package to be able to create server executable

---
+++ cmd/server/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/baturalpk/photo-bucket/api"
+	"github.com/baturalpk/photo-bucket/clients/entclient"
+	"github.com/baturalpk/photo-bucket/clients/s3client"
+	"github.com/baturalpk/photo-bucket/config"
+)
+
+func main() {
+	config.LoadConfigurations()
+
+	if err := s3client.InitConnection(); err != nil {
+		log.Fatalf("s3: init: error: %s\n", err)
+	}
+
+	if err := entclient.InitConnection(); err != nil {
+		log.Fatalf("ent: init: error: %s\n", err)
+	}
+
+	if err := api.Serve(config.Get().Server.Port); err != nil {
+		entclient.CloseConnection()
+		log.Fatalln(err)
+		return
+	}
+}
