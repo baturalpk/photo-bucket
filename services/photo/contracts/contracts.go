39be2ebcb76cd29e46860d5cf30c5281891b010c
baturalpk <baturalp.kiziltan@outlook.com>
2022-03-12 22:44:37 +0300

    refactor(photo): Re-implement PostNewPhoto handler
    [+] Updated JSON tags of contract struct: NewPhoto

--- services/photo/contracts/contracts.go
+++ services/photo/contracts/contracts.go
@@ -1,15 +1,14 @@
 package contracts
 
 import (
-	"image"
-
 	"github.com/google/uuid"
+	"image"
 )
 
 type NewPhoto struct {
-	OwnerID     uuid.UUID `json:"owner_id" validate:"required"`
-	Tags        []string  `json:"tags"`
-	Description string    `json:"description"`
-	Data        image.Image
-	Format      string
+	OwnerID     uuid.UUID   `json:"owner_id"`
+	Tags        []string    `json:"tags"`
+	Description string      `json:"description"`
+	Data        image.Image `json:"data"`
+	Format      string      `json:"format"`
 }
--- services/photo/handlers.go
+++ services/photo/handlers.go
@@ -1,10 +1,12 @@
 package photo
 
 import (
+	"fmt"
+	"image"
+	"math"
 	"net/http"
 
 	"github.com/baturalpk/photo-bucket/services/photo/contracts"
-	"github.com/baturalpk/photo-bucket/utils/validator"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 )
@@ -19,15 +21,41 @@ func NewHandlers(photoRepo *repository) *handlers {
 	}
 }
 
-func (h handlers) PostNewPhoto(c *fiber.Ctx) error { // TODO: ! Handle image data extraction explicitly, set format, etc...
+func (h handlers) PostNewPhoto(c *fiber.Ctx) error {
 	in := new(contracts.NewPhoto)
 	if err := c.BodyParser(in); err != nil {
 		return err
 	}
-	if err := validator.GetValidatorInstance().Struct(in); err != nil {
+	ownerStr := c.FormValue("owner_id")
+	if ownerStr == "" {
+		return c.Status(http.StatusBadRequest).SendString("no 'owner_id' indicated")
+	}
+	owner, err := uuid.Parse(ownerStr)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).SendString("'owner_id' must be uuidV4 type")
+	}
+	in.OwnerID = owner
+	photo, err := c.FormFile("photo")
+	if err != nil {
 		return err
 	}
-	_, err := h.repo.Create(c.UserContext(), *in)
+	if photo.Size > (30 << 20) { // 30 MB upload limit
+		return c.Status(http.StatusRequestEntityTooLarge).SendString(
+			fmt.Sprintf("photo size must not exceed %d MB",
+				(30<<20)/int(math.Pow(10, 6))),
+		)
+	}
+	photoFile, err := photo.Open()
+	if err != nil {
+		return err
+	}
+	img, format, err := image.Decode(photoFile)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).SendString("corrupted photo file")
+	}
+	in.Data = img
+	in.Format = format
+	_, err = h.repo.Create(c.UserContext(), *in)
 	return err
 }
 
